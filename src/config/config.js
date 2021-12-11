696c0d65e06c34f0fceb0237aa02f7af4991e443
PirateTosh <srs.paritosh7@gmail.com>
2021-12-12 01:19:33 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,6 +10,6 @@ export const EventId = "CSkW4fMnZzggCi4PS";
 export const AppTItle = "Raj & Simran's Italian Saga Wedding";
 export const eventTitle = "Raj & Simran's Italian Saga Wedding";
 export const AppShortName = "TheKD";
-export const AppURL = "https://softechlife.thekeyevent.in";
+export const AppURL = "";
 export const isMobile = window.innerWidth <= 500;
 
