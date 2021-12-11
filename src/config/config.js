0795d7b92a216d9c871798c5d1044120b6118e11
PirateTosh <srs.paritosh7@gmail.com>
2021-12-12 01:01:32 +0530

    PWA of pilot project KD

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+// export const EventId = "cQfC27MHoMqbzNpNw";
+// export const AppTItle = "VisionIT 2020 Conference";
+// export const eventTitle = "VisionIT 2020 Conference";
+// export const AppShortName = "SoftechLife";
+// export const AppURL = "https://softechlife.thekeyevent.in";
+// export const isMobile = window.innerWidth <= 500;
+
+
+export const EventId = "CSkW4fMnZzggCi4PS";
+export const AppTItle = "Raj & Simran's Italian Saga Wedding";
+export const eventTitle = "Raj & Simran's Italian Saga Wedding";
+export const AppShortName = "TheKD";
+export const AppURL = "https://softechlife.thekeyevent.in";
+export const isMobile = window.innerWidth <= 500;
+
